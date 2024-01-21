ac902c12a6c47447b4a9c08ae52bebd9a0082666
FrancoMiranda <francomirandaorozco@gmail.com>
2024-01-20 21:30:18 -0500

    error de slides

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   slides=[
-   /* {
+   {
+      mosaico:"",
       id_slide_css:"slide_inicio",
       div_titulo_css:"div_titulo",
       titulo_css:"titulo_inicio",
@@ -27,26 +28,27 @@ export class HomePage {
       imagen_1:"assets/img/hincha1.png",
       
       
-    },*/
+    },
     {
+      mosaico:"fondo",
       id_slide_css:"slides2",
       fondo_css:"fondo",
-      div_titulo_css:"div_titulo",
-      titulo_css:"titulo_inicio",
+      div_titulo_css:"div_titulo-b",
+      titulo_css:"titulo_inicio-b",
       titulo_1: "Esta es nuestra app",
       p1_css:"p1",
       parrafo_1:"Accede a un mundo de eventos de forma facil y seguro",
-      section_img_css:"logo",
-      class_img_logo_css:"imagen_logo",
+      section_img_css:"logo-b",
+      class_img_logo_css:"imagen_logo-b",
       img_logo:"assets/img/iconos.svg",
       titulo_2: "",
       p2_css:"p2",
-      parrafo_2:"Todo al alcance de tu mano y muy facil"
-  /*    section_imagenes_css:"position",
-      class_imagenes:"hincha",
+      parrafo_2:"Todo al alcance de tu mano y muy facil",
+      section_imagenes_css:"position-b",
+      class_imagenes:"hincha-b",
       imagen_3:"",
       imagen_2:"",
-      imagen_1:"",*/
+      imagen_1:""
     }
   ]
   constructor() {}
