07cad4a0a1ec1c6e1bbb8740cc4b96714855a1f8
FrancoMiranda <francomirandaorozco@gmail.com>
2024-01-20 20:47:19 -0500

    segundo slide

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,44 +7,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   slides=[
-    {
-      titulo_1: "BIENVENIDOS",
-      titulo_2: "aqui va texto",
-      parrafo_1:"",
-      parrafo_2:"",
+   /* {
       id_slide_css:"slide_inicio",
       div_titulo_css:"div_titulo",
       titulo_css:"titulo_inicio",
+      titulo_1: "BIENVENIDOS",
+      p1_css:"",
+      parrafo_1:"",
       section_img_css:"logo",
       class_img_logo_css:"imagen_logo",
       img_logo:"assets/img/app_logo.svg",
+      titulo_2: "aqui va texto",
+      p2_css:"",
+      parrafo_2:"",
       section_imagenes_css:"position",
       class_imagenes:"hincha",
       imagen_3:"assets/img/hincha3.png",
       imagen_2:"assets/img/hincha2.png",
       imagen_1:"assets/img/hincha1.png",
-      p1_css:"",
-      p2_css:""
-    },
+      
+      
+    },*/
     {
-      titulo_1: "Esta es nuestra app",
-      titulo_2: "aqui va texto",
-      parrafo_1:"Accede a un mundo de eventos de forma facil y seguro",
-      parrafo_2:"Todo al alcance de tu mano y muy facil",
-      id_slide_css:"slides",
+      id_slide_css:"slides2",
+      fondo_css:"fondo",
       div_titulo_css:"div_titulo",
       titulo_css:"titulo_inicio",
+      titulo_1: "Esta es nuestra app",
+      p1_css:"p1",
+      parrafo_1:"Accede a un mundo de eventos de forma facil y seguro",
       section_img_css:"logo",
       class_img_logo_css:"imagen_logo",
       img_logo:"assets/img/iconos.svg",
-      section_imagenes_css:"",
+      titulo_2: "",
+      p2_css:"p2",
+      parrafo_2:"Todo al alcance de tu mano y muy facil"
+  /*    section_imagenes_css:"position",
       class_imagenes:"hincha",
-      imagen_1:"",
-      imagen_2:"",
       imagen_3:"",
-      p1_css:"p1",
-      p2_css:"p2"
-      
+      imagen_2:"",
+      imagen_1:"",*/
     }
   ]
   constructor() {}
